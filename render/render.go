3f92733cdfc58894d214562a54a6b630c78a854b
mlange-42 <martin_lange_@gmx.net>
2024-05-31 00:22:29 +0200

    add text info to panels

--- render/render.go
+++ render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/mlange-42/tom/api"
@@ -29,13 +30,24 @@ func (r *Renderer) Day(index int) string {
 
 	_ = boxWidth
 
+	codes := r.data.GetSixHourly(api.HourlyWeatherCode)
+	temp := r.data.GetSixHourly(api.HourlyTemp)
+	appTemp := r.data.GetSixHourly(api.HourlyApparentTemp)
+	precip := r.data.GetSixHourly(api.HourlyPrecip)
+	precipProb := r.data.GetSixHourly(api.HourlyPrecipProb)
+	wind := r.data.GetSixHourly(api.HourlyWindSpeed)
+	windDir := r.data.GetSixHourly(api.HourlyWindDir)
+	clouds := r.data.GetSixHourly(api.HourlyCloudCover)
+	humidity := r.data.GetSixHourly(api.HourlyRH)
+
 	for i := 0; i < 4; i++ {
 		idx := index + i
 
-		code := int(r.data.GetHourly(api.HourlyWeatherCode)[idx])
+		code := int(codes[idx])
 		codeProps, ok := data.WeatherCodes[code]
 		if !ok {
-			codeProps = data.WeatherCodes[0]
+			panic(fmt.Sprintf("unknown weather code %d", code))
+			//codeProps = data.WeatherCodes[0]
 		}
 
 		x := 1 + (boxWidth+1)*i
@@ -45,13 +57,17 @@ func (r *Renderer) Day(index int) string {
 
 		text := []string{
 			fmt.Sprintf("%-5s %s", r.data.SixHourlyTime[idx].Format(api.TimeLayout), codeProps.Name),
+			fmt.Sprintf("%2d (%2d) °C", int(math.Round(temp[idx])), int(math.Round(appTemp[idx]))),
+			fmt.Sprintf("%2dmm (%3d%%)", int(math.Round(precip[idx])), int(math.Round(precipProb[idx]))),
+			fmt.Sprintf("%3dkm/h %-2s", int(math.Round(wind[idx])), api.Direction(windDir[idx])),
+			fmt.Sprintf("%3d%%CC %3d%%RH", int(math.Round(clouds[idx])), int(math.Round(humidity[idx]))),
 		}
 		symWidth := len(codeProps.SymbolRunes[0])
 		x += 1
 		for j, line := range text {
 			maxLen := boxWidth - (symWidth + 1)
 			if j == 0 {
-				maxLen = boxWidth
+				maxLen = boxWidth - 1
 			}
 			len := MinInt(maxLen, len(line))
 			copy(layout[j+yOffset][x:x+len], []rune(line[:len]))
