23d39ec3bc977e34d0d066979e64a8fc4f201609
mlange-42 <martin_lange_@gmx.net>
2024-06-01 23:44:35 +0200

    set up basic CLI

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/gdamore/tcell/v2 v2.7.4
 	github.com/rivo/tview v0.0.0-20240524063012-037df494fb76
+	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.9.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -12,10 +13,12 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/term v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,37 +1,65 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/mlange-42/tom/app"
 	"github.com/mlange-42/tom/config"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatal("please specify a location")
+	if err := rootCommand().Execute(); err != nil {
+		fmt.Printf("ERROR: %s\n", err.Error())
+		fmt.Print("\nRun `tom -h` for help!\n\n")
+		os.Exit(1)
 	}
-	location := strings.ToLower(os.Args[1])
+}
 
-	forceApi := strings.HasSuffix(location, "?")
-	location = strings.TrimSuffix(location, "?")
+// rootCommand sets up the CLI
+func rootCommand() *cobra.Command {
+	root := cobra.Command{
+		Use:           "tom",
+		Short:         "Terminal for Open-Meteo.",
+		Long:          `Terminal for Open-Meteo.`,
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Args:          cobra.ArbitraryArgs,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return fmt.Errorf("please specify a location")
+			}
+			location := strings.ToLower(strings.Join(args, " "))
 
-	cached, err := config.LoadLocations()
-	if err != nil {
-		log.Fatal(err)
+			forceApi := strings.HasSuffix(location, "?")
+			location = strings.TrimSuffix(location, "?")
+
+			cached, err := config.LoadLocations()
+			if err != nil {
+				return err
+			}
+
+			coords, ok := cached[location]
+			if ok && !forceApi {
+				a := app.New(strings.ToTitle(location), coords)
+				a.Run()
+				return nil
+			}
+
+			a := app.NewLocationDialog(location, cached)
+			if err := a.Run(); err != nil {
+				return err
+			}
+			return nil
+		},
 	}
 
-	coords, ok := cached[location]
-	if ok && !forceApi {
-		a := app.New(strings.ToTitle(location), coords)
-		a.Run()
-		return
-	}
+	root.Flags().SortFlags = false
 
-	a := app.NewLocationDialog(location, cached)
-	if err := a.Run(); err != nil {
-		log.Fatal(err)
-	}
+	return &root
 }
