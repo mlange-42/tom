780c162f62bff1e1910d1a1a9f237c7696979690
mlange-42 <martin_lange_@gmx.net>
2024-06-02 17:17:11 +0200

    implement braille canvas

---
+++ render/canvas.go
@@ -0,0 +1,81 @@
+package render
+
+import "strings"
+
+const brailleWidth = 2
+const brailleHeight = 4
+
+type Braille [2][4]uint8
+
+func (b *Braille) Rune() rune {
+	littleEndian := [8]uint8{
+		b[0][0], b[0][1], b[0][2],
+		b[1][0], b[1][1], b[1][2],
+		b[0][3], b[1][3],
+	}
+	var v = 0
+	for i, x := range littleEndian {
+		v += int(x) << i
+	}
+	return rune(v) + '\u2800'
+}
+
+type Canvas struct {
+	width  int
+	height int
+	grid   [][]Braille
+}
+
+func NewCanvas(w, h int) *Canvas {
+	grid := make([][]Braille, h)
+	for i := range grid {
+		grid[i] = make([]Braille, w)
+	}
+	return &Canvas{
+		width:  w,
+		height: h,
+		grid:   grid,
+	}
+}
+
+func (c *Canvas) Dims() (int, int) {
+	return c.width * brailleWidth, c.height * brailleHeight
+}
+
+func (c *Canvas) Set(x, y int, v bool) {
+	col, xx := x/brailleWidth, x%brailleWidth
+	row, yy := y/brailleHeight, y%brailleHeight
+
+	b := &c.grid[row][col]
+	if v {
+		b[xx][yy] = 1
+	} else {
+		b[xx][yy] = 0
+	}
+}
+
+func (c *Canvas) Runes() [][]rune {
+	runes := make([][]rune, c.height)
+
+	for i, row := range c.grid {
+		rowRunes := make([]rune, len(row))
+		for j, r := range row {
+			rowRunes[j] = r.Rune()
+		}
+		runes[i] = rowRunes
+	}
+	return runes
+}
+
+func (c *Canvas) String() string {
+	lines := make([]string, c.height)
+
+	for i, row := range c.grid {
+		builder := strings.Builder{}
+		for _, r := range row {
+			builder.WriteRune(r.Rune())
+		}
+		lines[i] = builder.String()
+	}
+	return strings.Join(lines, "\n")
+}
---
+++ render/canvas_test.go
@@ -0,0 +1,27 @@
+package render_test
+
+import (
+	"testing"
+
+	"github.com/mlange-42/tom/render"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCanvas(t *testing.T) {
+	c := render.NewCanvas(10, 4)
+
+	sx, sy := c.Dims()
+
+	assert.Equal(t, 20, sx)
+	assert.Equal(t, 16, sy)
+
+	c.Set(2, 0, true)
+	c.Set(3, 0, true)
+	c.Set(2, 1, true)
+
+	runes := c.Runes()
+	text := c.String()
+
+	assert.Equal(t, '⠋', runes[0][1])
+	assert.Equal(t, '⠋', []rune(text)[1])
+}
